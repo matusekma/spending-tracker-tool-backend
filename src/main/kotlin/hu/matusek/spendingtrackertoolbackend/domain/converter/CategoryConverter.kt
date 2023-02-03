4f27f44b2e974c9c823a5f0053b424a62190cd59
Marton Matusek <matusekma@gmail.com>
2023-02-03 14:06:21 +0100

    feat(transaction): create CategoryConverter component to convert lowercase category string to enum automatically

---
+++ src/main/kotlin/hu/matusek/spendingtrackertoolbackend/domain/converter/CategoryConverter.kt
@@ -0,0 +1,14 @@
+package hu.matusek.spendingtrackertoolbackend.domain.converter
+
+import hu.matusek.spendingtrackertoolbackend.domain.Category
+import org.springframework.core.convert.converter.Converter
+import org.springframework.stereotype.Component
+
+
+@Component
+class CategoryConverter : Converter<String, Category> {
+
+    override fun convert(categoryString: String): Category? =
+        if (categoryString.isBlank()) null else Category.valueOf(categoryString.uppercase())
+
+}
